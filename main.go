fbde46a6093b92d4c1cf3d0170844cff723bf57b
Florian Rand <florian.rand@gmail.com>
2022-07-14 09:03:34 +0200

    Update main to use cmd

--- main.go
+++ main.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-
-	"github.com/flrnd/parrot/db"
-	"github.com/flrnd/parrot/util"
+	"github.com/flrnd/parrot/cmd"
 )
 
 func main() {
-	dbPath := db.Path()
-	database, err := sql.Open("sqlite3", dbPath)
-	util.Check(err)
-
-	length := 8
-	words := make([]string, length)
-
-	for i := 0; i < length; i++ {
-		dices := util.RollDices(5, 6)
-		id := util.DicesToNumber(dices)
-
-		word := db.Find(id, database)
-		words[i] = word
-	}
-	defer database.Close()
-
-	passphrase := util.GeneratePassphrase(words, "-")
-	fmt.Println(passphrase)
+	cmd.Execute()
 }
