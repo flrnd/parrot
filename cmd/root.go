f8feba786201fcb80dbbeb106ea1c5b2aa757003
Florian Rand <florian.rand@gmail.com>
2022-07-14 09:05:53 +0200

    Refactor root with a more idiomatic code (also exit)

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,17 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{Use: "parrot"}
 
 func Execute() {
-	err := rootCmd.Execute()
-
-	if err != nil {
-		return
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
